66bd8eff3da7b0f23f0b4bcacff41b18e281687c
ccviitatiana <ffloowf@gmail.com>
2024-02-02 16:58:49 -0500

    commit _form

--- resources/views/cats/_form.blade.php
+++ resources/views/cats/_form.blade.php
@@ -5,18 +5,14 @@
 <label class="uppercase text-gray-100 text-xs">Contenido</label>
 <textarea name="body" id="" rows="5" class="rounded bg-gray-900 border-gray-800 w-full mb-4">{{ @$cat->body }}</textarea>
 
-<select class="select_raza" name="raza">
+<label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seleccione raza</label>
+<select id="countries"
+    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     <option value="Abisinio">Abisinio</option>
     <option value="Abisinio">Abisinio</option>
     <option value="Americano de pelo duro">Americano de pelo duro</option>
 </select>
-
-<script type="text/javascript">
-    $(document).ready(function() {
-        $('.select_raza').select2();
-    });
-</script>
-
+<br>
 <label class="uppercase text-gray-100 text-xs">Nombre gato</label>
 <textarea name="body" id="" rows="5" class="rounded bg-gray-900 border-gray-800 w-full mb-4">{{ @$cat->body }}</textarea>
 
